3797f27df53780715850505b19992cdecee61a1f
BlueDesi <profejaviersalinas@gmail.com>
2025-06-27 20:44:07 -0300

    Se agrega panel

---
+++ Pañol/Controllers/PanelController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pañol.Controllers
+{
+    public class PanelController : Controller
+    {
+        // GET: Panel
+        public ActionResult PanelAdmin()
+        {
+            return View();
+        }
+    }
+}
